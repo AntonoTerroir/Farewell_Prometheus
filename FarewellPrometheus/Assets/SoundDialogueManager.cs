71f3d3f56e4df95634e29e4d719fa8d870864585
Antonin Terrien <anto.terrien@gmail.com>
2020-02-12 11:56:57 +0100

    Voice Dialogue Fonctionnel

--- FarewellPrometheus/Assets/SoundDialogueManager.cs
+++ FarewellPrometheus/Assets/SoundDialogueManager.cs
@@ -4,11 +4,61 @@ using UnityEngine;
 
 public class SoundDialogueManager : MonoBehaviour
 {
-    private AkSwitch Switch;
-    public void DialogueTriggerSwitch()
+    public void DialogueTriggerSwitch01()
     {
-        Debug.Log(Switch + "switch set");
-        AkSoundEngine.SetSwitch(, Switch, gameObject);
+        Debug.Log("01 switch set");
+        AkSoundEngine.SetSwitch("Volga_Dial_P1", "P1_01", gameObject);
         AkSoundEngine.PostEvent("Volga_Dial_P1", gameObject);
     }
+    public void DialogueTriggerSwitch02()
+    {
+        Debug.Log("02 switch set");
+        AkSoundEngine.SetSwitch("Volga_Dial_P1", "P1_02", gameObject);
+        AkSoundEngine.PostEvent("Volga_Dial_P1", gameObject);
+    }
+    public void DialogueTriggerSwitch03()
+    {
+        Debug.Log("03 switch set");
+        AkSoundEngine.SetSwitch("Volga_Dial_P1", "P1_03", gameObject);
+        AkSoundEngine.PostEvent("Volga_Dial_P1", gameObject);
+    }
+    public void DialogueTriggerSwitch04()
+    {
+        Debug.Log("04 switch set");
+        AkSoundEngine.SetSwitch("Volga_Dial_P1", "P1_04", gameObject);
+        AkSoundEngine.PostEvent("Volga_Dial_P1", gameObject);
+    }
+    public void DialogueTriggerSwitch05()
+    {
+        Debug.Log("05 switch set");
+        AkSoundEngine.SetSwitch("Volga_Dial_P1", "P1_05", gameObject);
+        AkSoundEngine.PostEvent("Volga_Dial_P1", gameObject);
+    }
+    public void DialogueTriggerSwitch06()
+    {
+        Debug.Log("06 switch set");
+        AkSoundEngine.SetSwitch("Volga_Dial_P1", "P1_06", gameObject);
+        AkSoundEngine.PostEvent("Volga_Dial_P1", gameObject);
+    }
+    public void DialogueTriggerSwitch07()
+    {
+        Debug.Log("07 switch set");
+        AkSoundEngine.SetSwitch("Volga_Dial_P1", "P1_07", gameObject);
+        AkSoundEngine.PostEvent("Volga_Dial_P1", gameObject);
+    }
+    public void DialogueTriggerSwitch08()
+    {
+        Debug.Log("08 switch set");
+        AkSoundEngine.SetSwitch("Volga_Dial_P1", "P1_08", gameObject);
+        AkSoundEngine.PostEvent("Volga_Dial_P1", gameObject);
+    }
+    public void DialogueContinue()
+    {
+        AkSoundEngine.PostEvent("Volga_Dial_P1", gameObject); //lance le son de dialogue suivant
+    }
+
+    /*public void DialogueStop()
+    {
+        AkSoundEngine.PostEvent("Volga_Dial_Stop_All", gameObject);  //arrête le son du dialogue en cours
+    }*/
 }
