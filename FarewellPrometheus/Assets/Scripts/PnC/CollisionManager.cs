792c293b7a50f26443009d95a0777f1974a13c9e
Maxime <maxime.debock@gmail.com>
2020-02-19 17:02:23 +0100

    Go package test click

---
+++ FarewellPrometheus/Assets/Scripts/PnC/CollisionManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    public GameObject[] tab;
+
+    public void test()
+    {
+        Debug.Log("c'est cliqué");
+    }
+
+    public void ChangeTab()
+    {
+        tab[0].SetActive(false);
+        Debug.Log("ça disparait");
+    }
+    /*private void OnMouseDown()
+    {
+        gameObject.GetComponent<SceneLoader>().LoadScene(0);
+        Debug.Log("t'as cliqué");
+    }*/
+}
